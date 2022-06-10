ce36d1360fd9d9ec334a90cfe0c23638b2451f06
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2022-06-10 22:44:49 +0200

    Add data/ and fs dependencie

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const fs = require('fs');
 const express = require('express');
+const path = require('path');
 const app = express();
 
+const dataDirectory = path.join(process.cwd(), 'data');
+
+function getMnlthData() {
+  return JSON.parse(fs.readFileSync(dataDirectory + '/data.json'));
+}
+
 app.get('/', (req, res) => {
-  res.send("GM WORLDDD!");
+  const data = getMnlthData();
+  res.send("GM WORLDDD!" + data.mnlth.floorPrice);
+});
+
+app.get('/mnlth', (req, res) => {
+  res.status(200).json({mnlthData: 'ok'});
+});
+
+app.get('/mnlth/floor', (req, res) => {
+  res.status(200).json({floor: 'ok'});
 });
 
 const port = 3000;
