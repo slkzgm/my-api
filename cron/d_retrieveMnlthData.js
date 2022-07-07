57ed3fe967be3885196d359f4294c0210eee84b6
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2022-07-07 15:16:45 +0200

    Debug

--- cron/d_retrieveMnlthData.js
+++ cron/d_retrieveMnlthData.js
@@ -79,6 +79,7 @@ const testBypass1 = async (browser) => {
   try {
     await page.setExtraHTTPHeaders({'Accept-Language': 'en'});
     await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0');
+    await page.setJavaScriptEnabled(true);
 
     await page.goto(url);
     await page.waitForSelector(selectors.floorPrice, {timeout});
