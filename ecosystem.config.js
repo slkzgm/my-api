f30741f4fb25ff72b3af5da01ec4815d1b8e54ce
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2022-09-21 00:05:36 +0200

    PPreparation for public release

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,43 +17,36 @@ module.exports = {
 		instances: 1,
 		exec_mode: "fork",
 		cwd: "/home/lukas/my-api"
-	},{
-		name: "forgingzsn-update",
-		script: "./scripts/forgingszn/update.js",
-		watch: "true",
-		autorestart: false,
-		cron_restart: "*/1 * * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	},{
-		name: "api-server",
-		script: "./server.js",
-		cwd: "/home/lukas/my-api",
-		watch: true
-	}, {
-		name: "mnlth-discord-bot",
-		script: "./index.js",
-		cwd: "/home/lukas/mnlth-discord-bot",
-		watch: true
-	}, {
-		name: "mintvial-discord-bot",
-		script: "./index.js",
-		cwd: "/home/lukas/mintvial-discord-bot",
-		watch: true
-	}, {
-		name: "refreshCollectionsData",
-		script: "./scripts/refreshCollectionsData.js",
-		watch: true,
-		autorestart: false,
-		cron_restart: "0 */2 * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	}, {
-		name: "rtfktrvl-twitterBot",
-		script: "./index.js",
-		watch: true,
-		cwd: "/home/lukas/rtfktrvl-twitter-bot"
-	}]
+	},
+		// {name: "forgingzsn-update", script: "./scripts/forgingszn/update.js", watch: "true", autorestart: false, cron_restart: "*/3 * * * *", instances: 1, exec_mode: "fork", cwd: "/home/lukas/my-api"},
+		{
+			name: "api-server",
+			script: "./server.js",
+			cwd: "/home/lukas/my-api",
+			watch: true
+		}, {
+			name: "mnlth-discord-bot",
+			script: "./index.js",
+			cwd: "/home/lukas/mnlth-discord-bot",
+			watch: true
+		}, {
+			name: "mintvial-discord-bot",
+			script: "./index.js",
+			cwd: "/home/lukas/mintvial-discord-bot",
+			watch: true
+		}, {
+			name: "refreshCollectionsData",
+			script: "./scripts/refreshCollectionsData.js",
+			watch: true,
+			autorestart: false,
+			cron_restart: "0 */2 * * *",
+			instances: 1,
+			exec_mode: "fork",
+			cwd: "/home/lukas/my-api"
+		}, {
+			name: "rtfktrvl-twitterBot",
+			script: "./index.js",
+			watch: true,
+			cwd: "/home/lukas/rtfktrvl-twitter-bot"
+		}]
 }
