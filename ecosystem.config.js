1222ba0af3832f2b58139edac5007db7e59a5ab1
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2023-06-20 00:24:23 +0200

    Updating and reformat ecosystem.config for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,67 +1,84 @@
 module.exports = {
-	apps : [{
-		name: "mnlth-update",
-		script: "./scripts/mnlth-database/update.js",
-		watch: "true",
-		autorestart: false,
-		cron_restart: "*/1 * * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	},{
-		name: "mintvial-update",
-		script: "./scripts/mintvials-database/update.js",
-		watch: "true",
-		autorestart: false,
-		cron_restart: "*/1 * * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	},{
-		name: "eggs-update",
-		script: "./scripts/clonexeggs-database/update.js",
-		watch: "true",
-		autorestart: false,
-		cron_restart: "*/1 * * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	},{
-		name: "cirl-update",
-		script: "./scripts/cirl-database/update.js",
-		watch: "true",
-		autorestart: false,
-		cron_restart: "*/1 * * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	},{
-		name: "dotSwoosh-update",
-		script: "./scripts/dotswoosh-database/update.js",
-		watch: "true",
-		autorestart: false,
-		cron_restart: "*/2 * * * *",
-		instances: 1,
-		exec_mode: "fork",
-		cwd: "/home/lukas/my-api"
-	},
-		// {name: "forgingzsn-update", script: "./scripts/forgingszn/update.js", watch: "true", autorestart: false, cron_restart: "*/3 * * * *", instances: 1, exec_mode: "fork", cwd: "/home/lukas/my-api"},
+	apps : [
+		{
+			name: "mnlth-update",
+			script: "./scripts/mnlth-database/update.js",
+			watch: "true",
+			autorestart: false,
+			cron_restart: "*/1 * * * *",
+			instances: 1,
+			exec_mode: "fork",
+			cwd: "/home/lukas/my-api"
+		},
+		{
+			name: "mintvial-update",
+			script: "./scripts/mintvials-database/update.js",
+			watch: "true",
+			autorestart: false,
+			cron_restart: "*/1 * * * *",
+			instances: 1,
+			exec_mode: "fork",
+			cwd: "/home/lukas/my-api"
+		},
+		{
+			name: "eggs-update",
+			script: "./scripts/clonexeggs-database/update.js",
+			watch: "true",
+			autorestart: false,
+			cron_restart: "*/1 * * * *",
+			instances: 1,
+			exec_mode: "fork",
+			cwd: "/home/lukas/my-api"
+		},
+		// {
+		// 	name: "cirl-update",
+		// 	script: "./scripts/cirl-database/update.js",
+		// 	watch: "true",
+		// 	autorestart: false,
+		// 	cron_restart: "*/1 * * * *",
+		// 	instances: 1,
+		// 	exec_mode: "fork",
+		// 	cwd: "/home/lukas/my-api"
+		// },
+		{
+			name: "dotSwoosh-update",
+			script: "./scripts/dotswoosh-database/update.js",
+			watch: "true",
+			autorestart: false,
+			cron_restart: "*/2 * * * *",
+			instances: 1,
+			exec_mode: "fork",
+			cwd: "/home/lukas/my-api"
+		},
+		// {
+		// 	name: "forgingzsn-update",
+		// 	script: "./scripts/forgingszn/update.js",
+		// 	watch: "true",
+		// 	autorestart: false,
+		// 	cron_restart: "*/3 * * * *",
+		// 	instances: 1,
+		// 	exec_mode: "fork",
+		// 	cwd: "/home/lukas/my-api"
+		// },
 		{
 			name: "api-server",
 			script: "./server.js",
 			cwd: "/home/lukas/my-api",
 			watch: true
-		}, {
+		},
+		{
 			name: "mnlth-discord-bot",
 			script: "./index.js",
 			cwd: "/home/lukas/mnlth-discord-bot",
 			watch: true
-		}, {
+		},
+		{
 			name: "mintvial-discord-bot",
 			script: "./index.js",
 			cwd: "/home/lukas/mintvial-discord-bot",
 			watch: true
-		}, {
+		},
+		{
 			name: "refreshCollectionsData",
 			script: "./scripts/refreshCollectionsData.js",
 			watch: true,
@@ -70,20 +87,36 @@ module.exports = {
 			instances: 1,
 			exec_mode: "fork",
 			cwd: "/home/lukas/my-api"
-		}, {
+		},
+		{
 			name: "rtfktrvl-twitterBot",
 			script: "./index.js",
 			watch: true,
 			cwd: "/home/lukas/rtfktrvl-twitter-bot"
-		}, {
-			name: "dotSwoosh-twitterBot",
+		},
+		// {
+		// 	name: "dotSwoosh-twitterBot",
+		// 	script: "./index.js",
+		// 	watch: true,
+		// 	cwd: "/home/lukas/dotSwoosh-twitter-bot"
+		// },
+		// {
+		// 	name: "MEV",
+		// 	script: "./stream-api-2.js",
+		// 	watch: true,
+		// 	cwd: "/home/lukas/MEV"
+		// },
+		{
+			name: "eggs-sniper",
 			script: "./index.js",
 			watch: true,
-			cwd: "/home/lukas/dotSwoosh-twitter-bot"
-		}, {
-			name: "MEV",
-			script: "./stream-api-2.js",
+			cwd: "/home/lukas/unclaimed-clones-sniper"
+		},
+		{
+			name: "bonusdao",
+			script: "./src/monitor.js",
 			watch: true,
-			cwd: "/home/lukas/MEV"
-		}]
+			cwd: "/home/lukas/bonusdao"
+		}
+	]
 }
