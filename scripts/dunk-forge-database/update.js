10b5c5e1a29b98b7a2102d903ca85e0b3d2148b4
slkzgm <contact@slkzgm.com>
2023-11-20 12:40:58 +0100

    Updating with contract ABI for dunkGhostForge

--- scripts/dunk-forge-database/update.js
+++ scripts/dunk-forge-database/update.js
@@ -1,14 +1,17 @@
 const {contract: dunkForgeContract} = require("../../lib/contracts/dunkForge");
+const {contract: dunkGhostForgeContract} = require("../../lib/contracts/dunkGhostForge");
 const {setForgedSupply} = require("../../lib/dunkForgeDatabase");
 
 (async () => {
     try {
         const supply = await dunkForgeContract.methods.forgedSupply().call();
+        const ghostSupply = await dunkGhostForgeContract.methods.forgedSupply().call();
 
         await setForgedSupply({
             og: supply.genesisAmounts,
             x: supply.cloneAmounts,
-            v: supply.publicAmounts
+            v: supply.publicAmounts,
+            ghost: ghostSupply
         });
     } catch (e) {
         console.error(e);
